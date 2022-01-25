0a57c85df10426493833b76099e3083b0e8bdbc3
KarinnaGorrono <karinna.gorrono@gmail.com>
2022-01-25 09:37:50 -0700

    push

--- PlanIt-Vue/server/services/NotesServices.js
+++ PlanIt-Vue/server/services/NotesServices.js
@@ -1,10 +1,9 @@
 import { dbContext } from '../db/DbContext'
 import { logger } from '../utils/Logger'
-import { BadRequest, Forbidden } from '../utils/Errors'
+import { BadRequest } from '../utils/Errors'
 
 class NotesService {
-
-    async getAll(query = {}) {
+    async getNotes(query = {}) {
         return await dbContext.Notes.find(query).populate('creator', 'name')
     }
 
@@ -24,7 +23,7 @@ class NotesService {
     async remove(noteId, userId) {
         const note = await this.getById(noteId)
         if (note.creatorId.toString() !== userId) {
-            throw new Forbidden('You are not aloud to delete this note')
+            throw new BadRequest('You may not edit this note')
         }
         await dbContext.Notes.findByIdAndDelete(noteId)
     }
@@ -32,11 +31,12 @@ class NotesService {
     async edit(body) {
         const note = await this.getById(body.id)
         if (note.creatorId.toString() !== body.creatorId) {
-            throw new Forbidden('You are not aloud to edit this note')
+            throw new BadRequest('You may not edit this note')
         }
         const updateNote = dbContext.Notes.findOneAndUpdate({ _id: body.id }, body, { new: true })
         return await updateNote
     }
 }
+
 
 export const notesService = new NotesService()
